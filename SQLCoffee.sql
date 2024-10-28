1794f65f8506b21f1b778469179c60de61c8c309
Nguyên Trần (Jenny) <138330946+nguathan7x@users.noreply.github.com>
2024-10-28 13:49:45 +0700

    Add files via upload

---
+++ SQLCoffee.sql
@@ -0,0 +1,101 @@
+﻿Create database QuanLyQuanCafe
+go
+
+use QuanLyQuanCafe
+go
+
+--Food
+--Table
+--FoodCategory
+--Account
+--Bill
+--BillInfo
+
+CREATE TABLE TableFood
+(
+	id INT PRIMARY KEY,
+	name NVARCHAR(100),  
+	status NVARCHAR(100) 	--Trống|| Có người
+)
+GO
+
+CREATE TABLE Account
+(
+	UserName NVARCHAR(100) COLLATE SQL_Latin1_General_CP1_CS_AS PRIMARY KEY ,
+	DisplayName NVARCHAR(100),
+	PassWord NVARCHAR(1000) COLLATE SQL_Latin1_General_CP1_CS_AS ,
+	Type INT  --1 admin 0:staff
+)
+GO
+
+CREATE TABLE FoodCategory
+(
+	id INT PRIMARY KEY,
+	name NVARCHAR(100) 
+)
+GO
+
+CREATE TABLE Food
+(
+	id INT PRIMARY KEY,
+	name NVARCHAR(100) ,
+	idCategory INT ,
+	price FLOAT ,
+
+	FOREIGN KEY (idCategory) REFERENCES dbo.FoodCategory(id)
+)
+GO
+
+CREATE TABLE BILL
+(
+	id INT PRIMARY KEY IDENTITY(1, 1),
+	DateCheckIn DATE, 
+	DateCheckOut DATE,
+	idTable INT ,
+	status int  --1: đã thanh toán, 0: chưa thanh toán
+	
+	FOREIGN KEY(idTable) REFERENCES dbo.TableFood(id)
+)
+GO
+
+CREATE TABLE BillInfo
+(
+	id INT PRIMARY KEY IDENTITY(1, 1),
+	idBill INT,
+	idFood INT ,
+	count INT ,
+
+	FOREIGN KEY (idBill) REFERENCES dbo.Bill(id),
+	FOREIGN KEY (idFood) REFERENCES dbo.Food(id)
+)
+GO
+
+
+
+INSERT INTO FOODCATEGORY VALUES (1,N'Rice')
+INSERT INTO FoodCategory VALUES (2,N'Wine')
+INSERT INTO FoodCategory VALUES (3,N'Coffee')
+
+INSERT INTO Account VALUES (N'ngoc20022607@gmail.com',N'Nguyen',123,1)
+
+
+
+CREATE TABLE Customers (
+	
+    CustomerGmail nvarchar(100) COLLATE SQL_Latin1_General_CP1_CS_AS Primary Key,
+    FullName NVARCHAR(100) NOT NULL,
+	Password NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NOT NULL,
+    PhoneNumber NVARCHAR(15) NOT NULL,
+	Status BIT NOT NULL DEFAULT 1,
+    RegistrationDate DATETIME NOT NULL DEFAULT GETDATE(),
+    Points INT NOT NULL DEFAULT 0, 
+);
+
+
+Insert into Customers values (1,Taylor@gmail.com,N'Taylor Swift',N'123','0123456789') 
+INSERT INTO Customers (CustomerGmail, FullName, Password, PhoneNumber)
+VALUES (N'customer@example.com', N'Nguyễn Văn A', N'123', N'0912345678')
+INSERT INTO Customers (CustomerGmail, FullName, Password, PhoneNumber)
+VALUES (N'Taylor@gmail.com',N'Taylor Swift',N'123',N'0123456789') 
+select* from customers
+
